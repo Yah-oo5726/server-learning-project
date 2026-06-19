7ca676ac64076c4e3b5274acd20d93c996b02f5c
Yah-oo5726 <arsalannimaamini@gmail.com>
2026-06-18 18:56:51 -0700

    moved all file serving things to baseurl/app/ and made baseurl/healthz return a OK status when the server is active

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import "net/http"
 
 func main() {
 	servemux := http.NewServeMux()
-	servemux.Handle("/", http.FileServer(http.Dir(".")))
+	servemux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	servemux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := http.Server{Handler: servemux, Addr: ":8080"}
 	server.ListenAndServe()
 }
