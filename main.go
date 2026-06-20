28e3ab53a72567f6646990864265bb62a48065c6
Yah-oo5726 <arsalannimaamini@gmail.com>
2026-06-19 23:32:50 -0700

    Added a few things for metrics and such

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int64
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
+	config := apiConfig{}
 	servemux := http.NewServeMux()
-	servemux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	servemux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	apphandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.StripPrefix("/app", http.FileServer(http.Dir("."))).ServeHTTP(w, r)
+	})
+	servemux.Handle("/app/", config.middlewareMetricsInc(apphandler))
+	servemux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+	servemux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hits: " + strconv.FormatInt(config.fileserverHits.Load(), 10)))
+	})
+	servemux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		config.fileserverHits.Store(0)
+		w.Write([]byte("Hits reset to 0"))
 	})
 	server := http.Server{Handler: servemux, Addr: ":8080"}
 	server.ListenAndServe()
