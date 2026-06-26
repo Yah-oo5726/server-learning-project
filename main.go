91eb210bd37ec91c628b5ea3120c4fd5f8e5af98
Yah-oo5726 <arsalannimaamini@gmail.com>
2026-06-25 21:32:27 -0700

    sql setup

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Yah-oo5726/server-learning-project/internal/database"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int64
+	db *database.Queries
 }
 
 func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -19,7 +27,16 @@ func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func main() {
-	config := apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	config := apiConfig{db: dbQueries}
 	servemux := http.NewServeMux()
 	apphandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.StripPrefix("/app", http.FileServer(http.Dir("."))).ServeHTTP(w, r)
@@ -28,18 +45,27 @@ func main() {
 	servemux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			fmt.Println("Error writing response:", err)
+		}
 	})
 	servemux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(fmt.Sprintf("<html>\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %d times!</p>\n</body>\n</html>", config.fileserverHits.Load())))
+		_, err := w.Write([]byte(fmt.Sprintf("<html>\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %d times!</p>\n</body>\n</html>", config.fileserverHits.Load())))
+		if err != nil {
+			fmt.Println("Error writing response:", err)
+		}
 	})
 	servemux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		config.fileserverHits.Store(0)
-		w.Write([]byte("Hits reset to 0\n"))
+		_, err := w.Write([]byte("Hits reset to 0\n"))
+		if err != nil {
+			fmt.Println("Error writing response:", err)
+		}
 	})
 	servemux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
 		type parameters struct {
